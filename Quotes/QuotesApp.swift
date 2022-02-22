b4897e9200a23b099337da8fb5c2dfaab25f9573
Leo Lu <llu@lcs.on.ca>
2022-02-22 17:09:28 -0500

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Leo Lu on 2022-02-22.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
